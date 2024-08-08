7e20e48d4b35f54f2c466d7526a56b3c235dafc4
oromadin <osromadin@gmail.com>
2024-08-08 19:44:01 +0000

    change figures and colors

--- src/Figures.php
+++ src/Figures.php
@@ -8,17 +8,20 @@ class Figures {
     
     header('Content-type: image/png');
     $png_image = imagecreate(300, 300);
-    $grey = imagecolorallocate($png_image, 229, 229, 229);
-    $green = imagecolorallocate($png_image, 128, 204, 204);
-    imagefilltoborder($png_image, 0, 0, $grey, $grey);
+    $red = imagecolorallocate($png_image, 244, 19, 34);
+    $yellow = imagecolorallocate($png_image, 244, 207, 0);
+    $blue = imagecolorallocate($png_image, 14, 30, 244);
+    imagefilltoborder($png_image, 0, 0, $red, $red);
 
-    imagefilledrectangle ($png_image, 20, 20, 80, 80, $green);     // SQUARE
-    imagefilledrectangle ($png_image, 100, 20, 280, 80, $green);   // RECTANGLE
-    imagefilledellipse ($png_image, 50, 150, 75, 75, $green);      // CIRCLE
-    imagefilledellipse ($png_image, 200, 150, 150, 75, $green);    // ELLIPSE
+    imagefilledrectangle ($png_image, 20, 20, 200, 80, $yellow);   // RECTANGLE
+    imagefilledrectangle ($png_image, 220, 20, 280, 80, $yellow);     // SQUARE
 
-    $poly_points = array(150, 200, 100, 280, 200, 280);
-    imagefilledpolygon ($png_image, $poly_points, 3, $green);      // POLYGON
+    imagefilledellipse ($png_image, 200, 150, 150, 75, $blue);    // ELLIPSE
+    imagefilledellipse ($png_image, 50, 150, 75, 75, $blue);      // CIRCLE
+    
+
+    //$poly_points = array(150, 200, 100, 280, 200, 280);
+    //imagefilledpolygon ($png_image, $poly_points, 3, $green);      // POLYGON
 
     imagepng($png_image);
     imagedestroy($png_image);
