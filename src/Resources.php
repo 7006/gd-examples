1ab6d59b723a4de4dff2e4d71a93c871920241f7
oromadin <osromadin@gmail.com>
2024-08-08 19:25:16 +0000

    add daily schedule

--- src/Resources.php
+++ src/Resources.php
@@ -5,6 +5,6 @@ namespace GdExample;
 class Resources {
   
   const IMAGE_PATH = __DIR__ . '/../resources/Table.png';
-  const IMAGE_EXP_PATH = __DIR__ . '/../resources/Exeption.png';
+  const IMAGE_NOENERGY_PATH = __DIR__ . '/../resources/NoEnergy.png';
   const FONT_PATH = __DIR__ . '/../resources/RobotoMono.ttf';
 }
--- src/Table.php
+++ src/Table.php
@@ -4,21 +4,75 @@ namespace GdExample;
 
 class Table {
 
-  public static function main() {
-              
-     $png_table = imagecreatefrompng(Resources::IMAGE_PATH);
-     $png_exp = imagecreatefrompng(Resources::IMAGE_EXP_PATH);
-     
-     header('Content-type: image/png');
-
-     imagecopymerge($png_table, $png_exp, 80, 39, 0, 0, 49, 39, 100);
-     imagecopymerge($png_table, $png_exp, 130, 79, 0, 0, 49, 39, 100);
-     imagecopymerge($png_table, $png_exp, 630, 79, 0, 0, 49, 39, 100);
-     imagecopymerge($png_table, $png_exp, 530, 199, 0, 0, 49, 39, 100);
-     imagecopymerge($png_table, $png_exp, 180, 279, 0, 0, 49, 39, 100);
-     imagepng($png_table);
+   public static function main() {
       
-     imagedestroy($png_table);
-     imagedestroy($png_exp);
-  }
+      $dstTable = imagecreatefrompng(Resources::IMAGE_PATH);
+      $srcNoEnergy = imagecreatefrompng(Resources::IMAGE_NOENERGY_PATH);
+      
+      $srcNoEnergyBox = self::getNoEnergyBox();
+      $dstPoints = self::getDstPoints();
+      foreach ($dstPoints as $dstPoint) {
+         imagecopymerge(
+            $dstTable, $srcNoEnergy, 
+            $dstPoint['dst_x'], $dstPoint['dst_y'], 
+            $srcNoEnergyBox['src_x'], $srcNoEnergyBox['src_y'], $srcNoEnergyBox['src_width'], $srcNoEnergyBox['src_height'], 
+            100
+         );
+      }
+
+      header('Content-type: image/png');
+      imagepng($dstTable);
+      imagedestroy($dstTable);
+      imagedestroy($srcNoEnergy);
+   }
+
+   private static function getDstPoints() {
+      return [
+         [
+             'dst_x' => 80,
+             'dst_y' => 39
+         ],
+         [
+            'dst_x' => 130,
+            'dst_y' => 79
+         ],
+         [
+            'dst_x' => 530,
+            'dst_y' => 119
+         ],
+         [
+            'dst_x' => 630,
+            'dst_y' => 79
+         ],
+         [
+            'dst_x' => 730,
+            'dst_y' => 79
+         ],
+         [
+            'dst_x' => 730,
+            'dst_y' => 239
+         ],
+         [
+            'dst_x' => 1030,
+            'dst_y' => 239
+         ],
+         [
+            'dst_x' => 1080,
+            'dst_y' => 239
+         ],
+         [
+            'dst_x' => 1130,
+            'dst_y' => 239
+         ]
+      ];
+   }
+
+   private static function getNoEnergyBox() {
+      return [
+         'src_x' => 0,
+         'src_y' => 0,
+         'src_width' => 49,
+         'src_height' => 39
+      ];  
+   }
 }
